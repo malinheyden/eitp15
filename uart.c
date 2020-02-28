6c8609cc141b996da365f8c9f0ef2596381011d6
Malin Heyden <malin.heyden@gmail.com>
2020-02-28 11:16:22 +0100

    Added interrupt enabling

--- uart.c
+++ uart.c
@@ -6,53 +6,50 @@
  */ 
 #include "uart.h"
 
-void uart_init(){
-	//Vad ska vi ha för baudrate? UBBRn 12 bitars reg, ekvation på sidan 240'
-	//Vad har xbee för baudrate?
-	//SET baud
-	// set frame
-	//enable tx or rx
-	//For interrupt driven also clear global interrupt flag
-	
-	
-	
+void uart_init()
+{	
 	//Asynchronous is default
-	//Set baud rate, high and low bits
-	//UBRR0H |= 
-	//UBRR0L |=
+	//Set baud rate, (high) and low bits
+	//Default baud rate 9600 -> UBRR0L = 103 
+	UBRR0L |= 103; //är detta ok?
 	
-	//Enable transmitter
-	UCSR0B |= (1<<TXEN0);
+	//Enable tx complete interrupt, enable transmitter
+	UCSR0B |= (1 << TXCIE0)|(1 << TXEN0);
 	
-	//set frame format: 8 bits of data, no parity right now
-	UCSR0B |= (1<<UCSZ00)|(1<<UCSZ01);
+	//set frame format: 8 bits of data, no parity, 1 stop bit.
+	UCSR0C |= (1 << UCSZ00)|(1 << UCSZ01);
 	
-	//Enable TX
-}
-
-void uart_send_data(uint8_t data){
 	
 }
 
-void uart_set_baudrate(int baudrate){
+void uart_send_data(uint8_t data)
+{
+	
+}
+
+void uart_set_baudrate(int baudrate)
+{
 	//UBRR0H = 
 }
 
-void uart_enable_even_parity(){
+void uart_enable_even_parity()
+{
 	//Clear parity bits
 	UCSR0C &=~((1<<UPM00)|(1<<UPM01));
 	//Set parity bits for even parity
 	UCSR0C |= (1<<UPM00);
 }
 
-void uart_enable_odd_parity(){
+void uart_enable_odd_parity()
+{
 	//Clear parity bits
 	UCSR0C &=~((1 << UPM00)|(1<<UPM01));
-	//Set parity bits for even parity
+	//Set parity bits for odd parity
 	UCSR0C |= (1<<UPM00)|(1<<UPM01);
 }
 
-void uart_disable_parity(){
+void uart_disable_parity()
+{
 	//Clear parity bits
 	UCSR0C &=~((1<<UPM00)|(1<<UPM01));	
 }
