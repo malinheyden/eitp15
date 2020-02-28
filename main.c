26fd2a819a05f64bcdfdbd9e9cd73e61df436a47
Malin Heyden <malin.heyden@gmail.com>
2020-02-28 11:16:54 +0100

    Added interrupt for uart transmit complete

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * Created: 2020-02-05 15:00:33
  * Author : ma5002he-s
  */ 
-#define F_CPU 16000000UL
+#define F_CPU 16000000UL //for debugging
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -21,7 +21,7 @@
 // Define leds East, North, West, South
 
 //PA
-//1 = grön, 2 = orange, 3 = röd
+//1 = green, 2 = orange, 3 = red
 #define LED_E_1 4
 #define LED_E_2 5
 #define LED_E_3 6
@@ -46,22 +46,20 @@
 
 volatile uint16_t adc_value;
 volatile uint8_t x_flag;
-volatile uint8_t y_flag;
 
 int main(void)
 {
-	//Configure as output
+	//Configure leds as output
     DDRA |= (1<<LED_E_1)|(1<<LED_E_2)|(1<<LED_E_3);
     DDRB |= (1<<LED_N_1)|(1<<LED_N_2)|(1<<LED_N_3)|(1<<LED_W_1)|(1<<LED_W_2)|(1<<LED_W_3);
 	DDRD |= (1<<LED_S_1)|(1<<LED_S_2)|(1<<LED_S_3); 
 
+	//Start adc and enable interrupts
 	adc_init();
 	sei();
-
 	adc_start();
 
-    while (1) 
-    {
+    while (1) {
 		//TODO kör kontinuerligt x sen y sen x i adc, sätt en flagga när omvandlingen är klar. Då körs en ifsats som tänder/släcker lampor
 
 		PORTB &=~ ((1 << LED_N_1)|(1 << LED_N_2)|(1 << LED_N_3));
@@ -91,24 +89,25 @@ int main(void)
 		else{
 			PORTD |=(1 << LED_S_1)|(1 << LED_S_2)|(1 << LED_S_3);
 		}
-
-		
-		/*PORTA |=(1<<LED_E_1)|(1<<LED_E_2)|(1<<LED_E_3);
-		_delay_ms(25);
-		PORTB ^=(1<<LED_N_1)|(1<<LED_N_2)|(1<<LED_N_3);
-		_delay_ms(50);
-		PORTB ^=(1<<LED_W_1)|(1<<LED_W_2)|(1<<LED_W_3);
-		_delay_ms(50);
-		PORTD ^=(1<<LED_S_1)|(1<<LED_S_2)|(1<<LED_S_3); 
-		_delay_ms(50);*/
     }
 }
 
 
-ISR(ADC_vect){
+ISR(ADC_vect)
+{
 
 	adc_value = ADC;
 	//adc_change_pin();
 	adc_start();
 
+}
+
+//Interrupt executes when last transmission is completed. Puts the next frame in tx buffer
+ISR(USART1_TX_vect)
+{
+	//Data can only be written to buffer if UDRE flag is set
+	if ((UCSR0A & (1 << UDRE0)) == (1 << UDRE0)){
+		uart_send_data();
+	}
+	
 }
